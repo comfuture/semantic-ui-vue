62152db782e378a100c2c2b7a558cdc13a7b39e7
comfuture <comfuture@gmail.com>
2017-02-12 02:26:45 +0900

    suggest docs structure

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,7 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-
 import 'semantic-ui-css/semantic.min.css'
 
-const app = new Vue({...App})
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import {routes} from './router'
+import Sidebar from './components/Sidebar.vue'
+import Page from './components/Page.vue'
+
+Vue.use(VueRouter)
+Vue.component('docs-sidebar', Sidebar)
+Vue.component('docs-page', Page)
+
+
+const router = new VueRouter({routes})
+
+const app = new Vue({
+  router,
+  ...App
+})
 app.$mount('#app')
---
+++ docs/src/pages/Introduction.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <docs-page>
+    <h2>Introduction</h2>
+    <div class="ui basic padded segment">
+      semantic-ui-vue is the unofficial Vue integration for Semantic UI .
+    </div>
+    <div class="ui basic padded segment">
+      <div role="list" class="ui list"><div role="listitem" class="item"><i aria-hidden="true" class="check icon"></i><div class="content"><!-- react-text: 150 -->jQuery Free<!-- /react-text --></div></div><div role="listitem" class="item"><i aria-hidden="true" class="check icon"></i><div class="content"><!-- react-text: 154 -->Declarative API<!-- /react-text --></div></div><div role="listitem" class="item"><i aria-hidden="true" class="check icon"></i><div class="content"><!-- react-text: 158 -->Augmentation<!-- /react-text --></div></div><div role="listitem" class="item"><i aria-hidden="true" class="check icon"></i><div class="content"><!-- react-text: 162 -->Shorthand Props<!-- /react-text --></div></div><div role="listitem" class="item"><i aria-hidden="true" class="check icon"></i><div class="content"><!-- react-text: 166 -->Sub Components<!-- /react-text --></div></div><div role="listitem" class="item"><i aria-hidden="true" class="check icon"></i><div class="content"><!-- react-text: 170 -->Auto Controlled State<!-- /react-text --></div></div></div>
+    </div>
+  </docs-page>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
