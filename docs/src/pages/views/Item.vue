7594138d5f4b7537fe566a2a2f839a7e6cdc0ce5
comfuture <comfuture@gmail.com>
2017-02-16 02:13:17 +0900

    add item docs page

---
+++ docs/src/pages/views/Item.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <docs-page>
+    <h1 class="ui header">Item</h1>
+    <div class="ui basic segment">
+      <ui-items divided>
+        <ui-item>
+          <ui-content header="Header only"></ui-content>
+        </ui-item>
+        <ui-item image="http://semantic-ui.com/images/avatar/large/stevie.jpg">
+          <ui-content header="Image and header">
+            <p slot="description">description</p>
+          </ui-content>
+        </ui-item>
+        <ui-item>
+          <ui-content header="Test">
+            <p slot="description">description</p>
+          </ui-content>
+        </ui-item>
+        <ui-item>
+          <ui-content header="Test 2">
+            <p slot="description">alskdjflaksjdf </p>
+          </ui-content>
+        </ui-item>
+      </ui-items>
+    </div>
+  </docs-page>
+</template>
+
+<script>
+export default {
+  name: 'docs-views-item'
+}
+</script>
