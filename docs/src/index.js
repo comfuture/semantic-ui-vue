539985fbbd1296ad48b95476338dbebe3a05ad72
comfuture <comfuture@gmail.com>
2017-02-16 02:13:43 +0900

    change routing path

--- docs/src/index.js
+++ docs/src/index.js
@@ -6,11 +6,12 @@ import App from './App.vue'
 import {routes} from './router'
 import Sidebar from './components/Sidebar.vue'
 import Page from './components/Page.vue'
+import Semantic from '../../src'
 
 Vue.use(VueRouter)
+Vue.use(Semantic)
 Vue.component('docs-sidebar', Sidebar)
 Vue.component('docs-page', Page)
-
 
 const router = new VueRouter({routes})
 
