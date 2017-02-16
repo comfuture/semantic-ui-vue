da2a6fafc23d5d18a95ce5694b443126bc1a2d90
comfuture <comfuture@gmail.com>
2017-02-16 10:50:11 +0900

    add missing webpack.lib.js

---
+++ build/webpack.lib.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var baseConfig = require('./webpack.base')
+module.exports = merge(baseConfig, {
+  entry: {
+    lib: './src/index.js'
+  },
+  output: {
+    path: './dist/',
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ],
+  devtool: '#source-map'
+})
