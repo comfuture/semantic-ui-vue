82435b2ac053816172a50e116fcbe07fc29d9819
comfuture <comfuture@gmail.com>
2017-02-19 01:18:59 +0900

    partial implement content component

--- src/components/common/content/Content.vue
+++ src/components/common/content/Content.vue
@@ -2,24 +2,30 @@
   <div class="content" :class="className">
     <div class="header" v-if="!!header">{{header}}</div>
     <slot></slot>
-    <slot name="meta" v-if="!hasDefaultSlot"></slot>
-    <slot name="description" v-if="!hasDefaultSlot"></slot>
+    <slot name="meta"></slot>
+    <slot name="description"></slot>
+    <slot name="extra"></slot>
   </div>
 </template>
 
 <script>
+import {SlotClass} from 'semantic/mixins'
+
 export default {
   name: 'ui-content',
+  mixins: [
+    SlotClass({
+      description: 'description',
+      meta: 'meta',
+      extra: 'extra'
+    })
+  ],
   props: {
     header: String,
     visible: Boolean,
     hidden: Boolean
   },
   computed: {
-    hasDefaultSlot() {
-      return this.$slots.hasOwnProperty('default') &&
-             this.$slots.default.length > 0
-    },
     className() {
       return {
         visible: this.visible,
