b1bbbda1fdf94d8be2ebc27c2dce8a3def383198
comfuture <comfuture@gmail.com>
2017-02-19 15:54:26 +0900

    add ui-divider component

--- src/components/elements/index.js
+++ src/components/elements/index.js
@@ -1,9 +1,11 @@
 import Button from './button'
 import Container from './container'
+import Divider from './divider'
 
 export default {
   install(Vue) {
     Vue.use(Button)
     Vue.use(Container)
+    Vue.use(Divider)
   }
 }
