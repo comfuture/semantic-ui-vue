268fd4c17bfa47eeb66085ee38c0294e9f95d62f
comfuture <comfuture@gmail.com>
2017-02-25 15:30:53 +0900

    add Array.prototype.reduce polyfill

--- src/helper.js
+++ src/helper.js
@@ -5,6 +5,30 @@ function unique(arr) {
   })
 }
 
+if (typeof Array.prototype.reduce !== "function") {
+  Array.prototype.reduce = function(callback, acc=null) {
+    if (!this) {
+      throw new TypeError("Array.prototype.reduce called on null or undefined")
+    }
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + " is not a function")
+    }
+    let length = this.length >>> 0
+    var start = 0
+    if (length < 1) {
+      throw new TypeError("Reduce of empty array with no initial value")
+    }
+    if (!acc) {
+      acc = this[0]
+      start = 1
+    }
+    for (var i = 0; i < length; i++) {
+      acc = callback(acc, this[i], i, this)
+    }
+    return acc
+  }
+}
+
 export function addClass(el, classes) {
   let classArray = classes.split(' ')
   if (el.classList) {
