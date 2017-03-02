68d60a8779a50d68eae39b9fc1fe6c2cc79e6080
gongzza <gz.us.to@gmail.com>
2017-03-02 15:19:08 +0900

    Update testcase example
    `eql`이 순서에도 민감하게 검사를 해서
    순서에 상관없는 `containDeep`로 수정하였습니다.
    
    `containDeep`은 포함되어 있는지만 확인 하기 때문에
    좀 더 정확하게 같은지를 확인하기 위해서 `length`를 사용하였습니다.

--- test/unit/specs/elements/Button.spec.js
+++ test/unit/specs/elements/Button.spec.js
@@ -5,23 +5,30 @@ describe('Button.vue', () => {
   const newVM = utils.newVM(Button)
   it('default className', () => {
     const vm = newVM()
-    vm.$.classes().should.be.eql(['ui', 'button'])
+
+    const cx = ['ui', 'button']
+    vm.$.classes().should.containDeep(cx)
   })
 
   it('should have red', () => {
     const vm = newVM({
       color: 'red'
     })
-    vm.$.classes().should.be.eql(['ui', 'button', 'red'])
+
+    const cx = ['ui', 'red', 'button']
+    vm.$.classes().should.containDeep(cx).which.length(cx.length)
   })
 
   it('should have icon', () => {
     const vm = newVM({
       icon: 'sign in'
     })
-    vm.$.classes().should.be.eql(['ui', 'button', 'icon'])
+
+    const cx = ['ui', 'icon', 'button']
+    vm.$.classes().should.containDeep(cx).which.length(cx.length)
 
     const $i = vm.$.children('i')
-    $i.classes().should.be.eql(['icon', 'sign', 'in'])
+    const iconCx = ['icon', 'sign', 'in']
+    $i.classes().should.containDeep(iconCx).which.length(iconCx.length)
   })
 })
