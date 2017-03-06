678c5abacd141c38b891efea5be31435279ca108
comfuture <comfuture@gmail.com>
2017-03-06 17:40:33 +0900

    add Example component

--- docs/src/index.js
+++ docs/src/index.js
@@ -6,12 +6,14 @@ import App from './App.vue'
 import {routes} from './router'
 import Sidebar from './components/Sidebar.vue'
 import Page from './components/Page.vue'
-import Semantic from '../../src'
+import Example from './components/Example.vue'
+import Semantic from 'semantic'
 
 Vue.use(VueRouter)
 Vue.use(Semantic)
 Vue.component('docs-sidebar', Sidebar)
 Vue.component('docs-page', Page)
+Vue.component('docs-example', Example)
 
 const router = new VueRouter({routes})
 
